16c585bd5214e02e14348badc0d837beb88c6a7e
Ryan Paul <rpaul@2u.com>
2018-09-15 16:51:15 -0400

    added pluralization

--- LIveSession3RCodeCLTFunctionWErrors.R
+++ LIveSession3RCodeCLTFunctionWErrors.R
@@ -3,7 +3,7 @@ SampDistSampMean = function(n = 100, simulations = 1000, Dist = "Norm", mean = 0
 {
   xbar_holder = numeric(n)
   TheSample = numeric(n)
-  for(i in 1:simulation)     # for loop to make "simulation" number of xbars
+  for(i in 1:simulations)     # for loop to make "simulation" number of xbars
   {
     if(Dist == "Norm") TheSample = rnorm(n,mean,sd)
     if(Dist == "Uni")  TheSample = runif(n)
