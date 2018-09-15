22c24c6ba4298100086f3fb35b80a152e5a5df1b
Ryan Paul <rpaul@2u.com>
2018-09-15 17:34:39 -0400

    fixed ttest code

--- Education_Conclusion_Sixers.r
+++ Education_Conclusion_Sixers.r
@@ -2,6 +2,6 @@ library("repmis")
 url <- "https://raw.githubusercontent.com/BivinSadler/UNIT-3-Repo/master/EducationData.csv"
 
 educationData <- repmis::source_data(url)
-t.test(Income2005 ~ factor(Educ), data=educationData, alternative="greater")
+t.test(Income2005 ~ factor(Educ), data=educationData, alternative="less")
 
-# There is insufficient to suggest that the mean income of group 16 is greater than 12, even though it is because the pvalue is 1 which is absurd and i'm doing something wrong.
+# The evidence suggests that the mean income of group 16 is greater than 12 (p-value < 2.2e-16).
